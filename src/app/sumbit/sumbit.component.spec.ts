5055e254796e07b99b67fe516e22a28569c2ca2c
RodolfoC-del <RODOLFCOLLINS@newhorizons.edu.do>
2021-12-16 04:39:29 +0000

    second-commit

---
+++ src/app/sumbit/sumbit.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SumbitComponent } from './sumbit.component';
+
+describe('SumbitComponent', () => {
+  let component: SumbitComponent;
+  let fixture: ComponentFixture<SumbitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SumbitComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SumbitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
